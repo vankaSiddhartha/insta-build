86bab5000a2aa69f692e90689d6d531854ce1a92
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-01-16 09:14:18 +0530

    Integrated Clerk authentication

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -28,7 +28,7 @@ export default function Page() {
         </a>
 
         <h2 class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to IntrvuAI 
+          Welcome to InstaBuild 
         </h2>
 
         <p class="mt-4 leading-relaxed text-white/90">
